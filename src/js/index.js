31e3f5bf9dc6df657ca1ca3ec93c92f12e5c82d4
stepanovamariia <mariia.alexsa@gmail.com>
2024-03-21 20:52:37 +0200

    final2

--- src/js/index.js
+++ src/js/index.js
@@ -12,6 +12,7 @@ const refs = {
 
 let currentPage = 1;
 const maxPage = 14;
+let lightbox;
 
 let options = {
   root: null,
@@ -19,37 +20,12 @@ let options = {
   threshold: 0,
 };
 
-let observer = new IntersectionObserver(onLoad, options);
-
-let lightbox;
-
-function onLoad(entries, observer) {
-  entries.forEach(async entry => {
-    if (entry.isIntersecting) {
-      try {
-        const inputData = refs.input.value.trim().toLowerCase();
-        const data = await serviceCardsInfo(inputData, currentPage);
-        const markup = createMarkup(data.hits);
-        refs.gallery.insertAdjacentHTML('beforeend', markup);
-        currentPage += 1;
-        lightbox.refresh();
-      } catch (err) {
-        console.log(err);
-      }
-
-      if (!entry.isIntersecting || currentPage >= maxPage) {
-        observer.unobserve(refs.target);
-      }
-    }
-  });
-}
-
 refs.form.addEventListener('submit', handleSubmit);
 
 async function handleSubmit(event) {
   event.preventDefault();
   refs.gallery.innerHTML = '';
-  currentPage = 1;
+  // currentPage = 1;
   const inputData = refs.input.value.trim().toLowerCase();
 
   if (inputData !== 'cat' && inputData !== 'dog') {
@@ -106,4 +82,33 @@ function createMarkup(arr) {
 </a>`
     )
     .join('');
+}
+
+let observer = new IntersectionObserver(onLoad, options);
+
+function onLoad(entries, observer) {
+  entries.forEach(async entry => {
+    if (entry.isIntersecting) {
+      const inputData = refs.input.value.trim().toLowerCase();
+
+      if (inputData !== 'cat' && inputData !== 'dog') {
+        refs.gallery.innerHTML = '';
+        Notiflix.Notify.failure("Please enter 'cat' or 'dog'");
+        return;
+      }
+      try {
+        const data = await serviceCardsInfo(inputData, currentPage);
+        const markup = createMarkup(data.hits);
+        refs.gallery.insertAdjacentHTML('beforeend', markup);
+        currentPage += 1;
+        lightbox.refresh();
+      } catch (err) {
+        console.log(err);
+      }
+
+      if (!entry.isIntersecting || currentPage >= maxPage) {
+        observer.unobserve(refs.target);
+      }
+    }
+  });
 }
